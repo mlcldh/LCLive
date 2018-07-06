378e996af8a0558c297db4f927337568513faafa
孟令超 <1228225993@qq.com>
2018-07-06 15:16:18 +0800

    增加基础模块。

---
+++ projects/LCBase/LCBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCBase
+//
+//  Created by MengLingChao on 2018/7/6.
+//  Copyright © 2018年 MengLingChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
